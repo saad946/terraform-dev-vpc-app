35e211dacff362dfc20ee037138e6fe8d209fecf
Saadullah Khan Warsi <saadullah946@gmail.com>
2023-07-26 19:43:04 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
-module "modules" {
+module "dev-vpc" {
   source  = "app.terraform.io/Rajhi-Kist/modules/aws"
-  version = "0.0.1"
-
+  version = "0.0.2"
   vpc_cidr_block                                      = var.vpc_cidr_block
   env                                                 = var.env
   region                                              = var.region
